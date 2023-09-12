9bdab5c094a3cac83a7bd53086deb39e3cf84db3
Irving Paim <irvingpaim@yahoo.com.br>
2023-09-11 21:34:22 -0300

    avançando em tipos

---
+++ 3_AVANCANDO_EM_TIPOS/index.js
@@ -0,0 +1,8 @@
+"use strict";
+// 1 - arrays
+let numbers = [1, 2, 3];
+numbers.push(5);
+console.log(numbers[2]);
+let family = ["Irving", "Leila"];
+family.push("Heitor");
+console.log(`${family[2]} nasceu do fruto do amor entre ${family[0]} e ${family[1]}`);
---
+++ 3_AVANCANDO_EM_TIPOS/index.ts
@@ -0,0 +1,13 @@
+// 1 - arrays
+
+let numbers: number[] = [1, 2, 3]
+
+numbers.push(5)
+
+console.log(numbers[2])
+
+let family: string[] = ["Irving", "Leila"]
+
+family.push("Heitor")
+
+console.log(`${family[2]} nasceu do fruto do amor entre ${family[0]} e ${family[1]}`)
