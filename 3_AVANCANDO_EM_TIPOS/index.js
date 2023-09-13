1f20d2fb152fd63d3e5ceee64149b67e8c5ceec2
Irving Paim <irvingpaim@yahoo.com.br>
2023-09-12 22:30:37 -0300

    Atualizado até tipos de retorno

--- 3_AVANCANDO_EM_TIPOS/index.js
+++ 3_AVANCANDO_EM_TIPOS/index.js
@@ -6,3 +6,23 @@ console.log(numbers[2]);
 let family = ["Irving", "Leila"];
 family.push("Heitor");
 console.log(`${family[2]} nasceu do fruto do amor entre ${family[0]} e ${family[1]}`);
+// 2 - Outra Sintaxe de Array
+const numbs = [100, 200];
+numbs.push(300);
+console.log(numbs);
+console.log(numbs[2]);
+// 3 - any
+const array = [1, "teste", true, [], { name: 'Irving' }];
+console.log(array);
+array.push([1, 2, 3]);
+console.log(array);
+// 4 - Parâmetros Tipados
+function soma(a, b) {
+    console.log(a + b);
+}
+soma(5, 5);
+const sum = (n1, n2) => console.log(n1 - n2);
+sum(10, 2);
+// 5 - Retorno da função
+const greeting = (name) => `Olá, ${name}`;
+console.log(greeting('Irving'));
--- 3_AVANCANDO_EM_TIPOS/index.ts
+++ 3_AVANCANDO_EM_TIPOS/index.ts
@@ -11,3 +11,45 @@ let family: string[] = ["Irving", "Leila"]
 family.push("Heitor")
 
 console.log(`${family[2]} nasceu do fruto do amor entre ${family[0]} e ${family[1]}`)
+
+// 2 - Outra Sintaxe de Array
+
+const numbs: Array<number> = [100, 200]
+
+numbs.push(300)
+
+console.log(numbs)
+
+console.log(numbs[2])
+
+// 3 - any
+
+const array: any = [1, "teste", true, [], {name: 'Irving'}]
+
+console.log(array)
+
+array.push([1, 2, 3])
+
+console.log(array)
+
+// 4 - Parâmetros Tipados
+
+function soma (a: number, b: number) {
+    console.log(a + b)
+}
+
+soma(5, 5)
+
+const sum = (n1: number, n2: number) => console.log(n1 - n2)
+
+sum(10, 2)
+
+// 5 - Retorno da função
+
+const greeting = (name: string): string => `Olá, ${name}`
+
+console.log(greeting('Irving'))
+
+
+
+
